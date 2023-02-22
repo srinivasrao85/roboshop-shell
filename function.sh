622e6f33b65ef866bbb92a1759f6493123d67cf2
Srinivasa Rao Kommanaboyina <125401403+srinivasrao85@users.noreply.github.com>
2023-02-23 00:24:13 +0530

    Add files via upload

--- function.sh
+++ function.sh
@@ -1,6 +1,10 @@
 ##Declare a function
 xyz() {
   echo Hello From Function
+  echo First Argumnet - $1
+  echo Second Arument - $2
+  echo All Arguents - $*
+  echo No of Arguments - $#
 }
 ## Call a function
-xyz
+xyz 456 321
