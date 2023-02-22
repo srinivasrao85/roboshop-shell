92a2a8dd11aff36167b03920618c5ad91fa7dfe8
Srinivasa Rao Kommanaboyina <125401403+srinivasrao85@users.noreply.github.com>
2023-02-23 01:32:48 +0530

    Add files via upload

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,12 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+source common.sh
+print_head "Set up Mongodb Repository"
+cp {code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
+print_head "Install Mongodb"
+yum install mongodb-org -y &>>{log_file}
+print_head "Enable Mongodb"
+systemctl enable mongod &>>{log_file}
+print_head "Start Mongodb Service"
+systemctl start mongod &>>{log_file}
 
 
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+#Update listen address from 127.0.int0.1 to 0.0.0.0 in /etc/mongod.conf
