57732d06099d7d4c927bf30e220cf40f219f29d3
Srinivasa Rao Kommanaboyina <125401403+srinivasrao85@users.noreply.github.com>
2023-02-22 22:15:11 +0530

    Add files via upload

--- roboshop.sh
+++ roboshop.sh
@@ -1,3 +1,4 @@
+code_dir=$(pwd)
 echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
 echo -e "\e[35mRemoving old content\e[0m"
@@ -8,9 +9,7 @@ echo -e "\e[35mExtracting downloaded frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 echo -e "\e[35m Copying nginx config for roboshop\e[0m"
-pwd
-ls -l
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[35m enabling nginx\e[0m"
 systemctl enable nginx
 echo -e "\e[35m restarting nginx\e[0m"
