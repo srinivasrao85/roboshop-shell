c0c7525c5ccb6504471cd77a796d2ae583a4037a
Srinivasa Rao Kommanaboyina <125401403+srinivasrao85@users.noreply.github.com>
2023-02-23 00:46:45 +0530

    Add files via upload

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 print_head(){
-  echo -e "\e[35m$1\e[0m"
+  echo -e "\e[34m$1\e[0m"
 }
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
